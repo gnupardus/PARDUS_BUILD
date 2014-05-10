f53e0715c085fd4a8a16a9bea0ce660b32d70dfd
Cavit VURAL <cavit.vural@yahoo.com>
2014-05-10 15:52:02 +0300

    KDE ' de pulseaudio autostart ' tan çıkarıldı. Açılış ve kapanışı cok geciktiriyor

--- files/pardus_config_kde.sh
+++ files/pardus_config_kde.sh
@@ -7,5 +7,9 @@ echo "KDE AYARLANIYOR"
 ###### Remove Debian Menu
 
 rm -f /etc/xdg/menus/debian-menu.menu
+
+## Remove pulseaudio from xdg autostart
+
+rm -f /etc/xdg/autostart/pulseaudio*
  
 echo "KDE AYARLANDI"
