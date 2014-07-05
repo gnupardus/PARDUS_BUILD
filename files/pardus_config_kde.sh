e19b3327cafbdf33496c51c6845633eb256cca70
Cavit VURAL <cavit.vural@yahoo.com>
2014-07-05 11:22:17 +0300

    3.0rc3 için hazırlık

--- files/pardus_config_kde.sh
+++ files/pardus_config_kde.sh
@@ -11,5 +11,8 @@ rm -f /etc/xdg/menus/debian-menu.menu
 ## Remove pulseaudio from xdg autostart
 
 rm -f /etc/xdg/autostart/pulseaudio-kde.desktop
+rm -f /etc/xdg/autostart/pulseaudio.desktop
+rm -f /etc/xdg/autostart/hplip-systray.desktop
+
  
 echo "KDE AYARLANDI"
