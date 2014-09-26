12c71b421b4e73c4c2f6af65b26ad8738fec34b0
Cavit VURAL <cavit.vural@yahoo.com>
2014-09-26 15:58:32 +0300

    live-build paketleri güncellendi

--- files/finish_install.sh
+++ files/finish_install.sh
@@ -39,7 +39,7 @@ fi
 
 # Remove packages
 update-rc.d live-installer remove
-DEBS="mint-translations pardus-installer-* live-boot live-boot-initramfs-tools live-config live-config-sysvinit live-tools"
+DEBS="mint-translations pardus-installer-* live-boot live-boot-initramfs-tools live-config live-config-sysvinit live-tools inxi live-config-systemd "
 ########## if [ "$PARDUS_DAGITIM" == "Sunucu" ]
 ########## then
     ########## DEBS=$DEBS" virtualbox-ose-guest-dkms"
